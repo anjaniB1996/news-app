776548fa2227c17b4efca12ff2d546104f033cff
Anjani <anjanibaghel1996@gmail.com>
2022-10-10 15:59:55 +0530

    newslist

--- src/App.js
+++ src/App.js
@@ -10,30 +10,25 @@ class News extends React.Component {
     async componentDidMount() {
       console.log("i am coponentdidmount");
       let res = await fetch(
-        `https://newsapi.org/v2/everything?q=${this.props.newsName}&apiKey=84f15556d3e14f9d88f91e0218bdd06a`
+        `https://newsapi.org/v2/everything?q=${this.props.newsName}&apiKey=39c3025e706146f99c1db7b6e2295f6e`
       );
       let data = await res.json();
       console.log(data, typeof data.articles, data.articles, typeof ar);
       let w = { width: "400px" };
       let arr = data.articles.map((p) => {
         return (
-          <div className="p-8">  
+          <div class="p-8">  
           {/* <!--Card 1--> */}
-         
-          <div className="max-w-sm rounded overflow-hidden shadow-lg">
-            <img className="w-full" src={p.urlToImage}/>
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl mb-2">{p.title}</div>
-              <p className="text-gray-700 text-base">
+          <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <img class="w-full" src={p.urlToImage}/>
+            <div class="px-6 py-4">
+              <div class="font-bold text-xl mb-2">{p.title}</div>
+              <p class="text-gray-700 text-base">
                 {p.description}
               </p>
-              <button className="font-bold text-xl mb-2 .text-primary"><a href={p.url}>Read more</a></button>
+              <button class="font-bold text-xl mb-2"> <a href={p.url}>Read more</a></button>
             </div>
-            <div className="px-6 pt-4 pb-2">
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#coding</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Akhilesh</span>
-              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#AI</span>
-            </div>
+    
           </div>
         </div>
         );
@@ -44,7 +39,7 @@ class News extends React.Component {
     render() {
       // this.setState({count:this.state.count+1})
       console.log(" i am render",this.state.count);
-      return  <div className="p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-1">{this.state.name1} </div>;
+      return  <div class="p-2 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-1">{this.state.name1} </div>;
   }}
 
 export default News;
